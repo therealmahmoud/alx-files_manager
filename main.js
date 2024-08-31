13a56b41f459d1ce3e13c8b71aed3637219451e4
therealmahmoud <moha91875460@gmail.com>
2024-08-31 18:55:34 +0300

    Updated

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
-import redisClient from "./utils/redis";
+import dbClient from './utils/db';
+
+const waitConnection = () => {
+    return new Promise((resolve, reject) => {
+        let i = 0;
+        const repeatFct = async () => {
+            await setTimeout(() => {
+                i += 1;
+                if (i >= 10) {
+                    reject()
+                }
+                else if(!dbClient.isAlive()) {
+                    repeatFct()
+                }
+                else {
+                    resolve()
+                }
+            }, 1000);
+        };
+        repeatFct();
+    })
+};
 
 (async () => {
-  console.log(redisClient.isAlive());             // Check if the Redis client is alive (connected)
-  console.log(await redisClient.get("myKey"));    // Try to get a value from Redis
-  await redisClient.set("myKey", 12, 5);          // Set a key in Redis with an expiration time
-  console.log(await redisClient.get("myKey"));    // Get the key's value after setting it
-
-  setTimeout(async () => {
-    console.log(await redisClient.get("myKey"));  // Try to get the key's value after it should have expired
-  }, 1000 * 10);  // Wait for 10 seconds
+    console.log(dbClient.isAlive());
+    await waitConnection();
+    console.log(dbClient.isAlive());
+    console.log(await dbClient.nbUsers());
+    console.log(await dbClient.nbFiles());
 })();
